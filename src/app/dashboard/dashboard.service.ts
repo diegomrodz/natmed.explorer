d420ac676bdf5655f28ba2c05bf05a10587a4eaf
Diego Rodrigues <diego.mrodrigues@outlook.com>
2017-03-25 14:53:24 -0300

    Dashboard Service

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -2,7 +2,25 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class DashboardService {
+  public question:any;
+  public answer:any;
 
-  constructor() { }
+  public hasData:boolean;
 
+  public setResponse(res:any) {
+    console.log(res)
+    this.question = res.question
+    this.answer = res.answer
+    this.hasData = true
+  }
+
+  public clear() {
+    this.question = undefined
+    this.answer = undefined
+    this.hasData = false
+  }
+
+  constructor() {
+    this.hasData = false
+  }
 }
