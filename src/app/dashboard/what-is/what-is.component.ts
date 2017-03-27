b128dfd8455f46f2241913f8c2e893dbb9630d62
Diego Rodrigues <diego.mrodrigues@outlook.com>
2017-03-26 22:00:37 -0300

    What Is Component

--- src/app/dashboard/what-is/what-is.component.ts
+++ src/app/dashboard/what-is/what-is.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { DashboardComponent } from './../dashboard.component';
 
 @Component({
   selector: 'app-what-is',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./what-is.component.scss']
 })
 export class WhatIsComponent implements OnInit {
+  @Input() answer:any;
+  @Input() parent:DashboardComponent;
 
   constructor() { }
 
