12fe0d97323bc63d8e87b1d2cd36b929f818e971
Diego Rodrigues <diego.mrodrigues@outlook.com>
2017-03-31 13:50:13 -0300

    Fixing the metadata and follow_up questions

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -3,7 +3,9 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class DashboardService {
   public question:any;
+  public metadata:any;
   public answer:any;
+  public follow_up:any;
 
   public hasData:boolean;
 
@@ -11,12 +13,16 @@ export class DashboardService {
     console.log(res)
     this.question = res.question
     this.answer = res.answer
+    this.metadata = res.metadata
+    this.follow_up = res.follow_up
     this.hasData = true
   }
 
   public clear() {
     this.question = undefined
     this.answer = undefined
+    this.metadata = undefined
+    this.follow_up = undefined
     this.hasData = false
   }
 
